a496b867f48266ad4573909677e85328e78c3335
CT <oil1005@naver.com>
2017-05-05 11:49:07 +0800

    app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.get('/',function (req,res) {
-  res.send('Hello world!');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function(){
   console.log('Server On!');
